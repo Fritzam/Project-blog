938a27f6f4ef40e2e07243edfb5ecdcc0bfbb4ec
Michal Fritza <Tornak9@gmail.com>
2024-06-14 22:20:23 +0200

    Posting logic

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-include "MainSite/main.html";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<header>
+    <span><a href="Logika/logowanie.html">Zaloguj sie</a></span>
+    <span><a href="Logika/rejestracja.html">Zarejestruj sie</a></span>
+    <span><</span>
+    <hr>
+</header>
+<body>
+</body>
+</html>
